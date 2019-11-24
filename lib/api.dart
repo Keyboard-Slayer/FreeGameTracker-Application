4be2317035a4c824c38e460130a773e85006973c
0v3rl0w <dalcjor@libmail.eu>
2019-11-24 17:47:25 +0100

    Fixed problems with null value on expiration date

--- lib/api.dart
+++ lib/api.dart
@@ -12,9 +12,17 @@ List<Widget> create_listing(Map<String, dynamic> json) {
 
   for(int i=0; i<json.keys.length; i++) {
     tmp = json[json.keys.elementAt(i)];
+    
     for(int j=0; j<tmp.length; j++) {
-      expiration = tmp[j]["expiration"].replaceAll(new RegExp(r'T'), ' at ');
-      expiration = expiration.substring(0, expiration.length-3);
+    
+      if(tmp[j]["expiration"] != null) {
+        expiration = tmp[j]["expiration"].replaceAll(new RegExp(r'T'), ' at ');
+        expiration = expiration.substring(0, expiration.length-3);
+        expiration = "Expiration: $expiration";
+      } else{
+        expiration = "No expiration date";
+      }
+
       games.add(
         Card(
           child: ListTile(
@@ -24,7 +32,7 @@ List<Widget> create_listing(Map<String, dynamic> json) {
                 width: 100,
               ),
               title: Text(tmp[j]["name"]),
-              subtitle: Text("Expiration $expiration"),
+              subtitle: Text(expiration),
               isThreeLine: true,
               trailing: OutlineButton(
                   onPressed: (){
